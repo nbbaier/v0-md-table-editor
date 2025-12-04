825f2931d290a2e1b33c85347ab46832d5c49aeb
Nico Baier <nico.baier@gmail.com>
2025-12-04 12:47:50 -0600

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel/serverless";
+
 export default defineConfig({
-	integrations: [
-		react(),
-		tailwind({
-			applyBaseStyles: false,
-		}),
+  integrations: [
+      react(),
+      tailwind({
+          applyBaseStyles: false,
+      }),
 	],
+
+  output: "server",
+  adapter: vercel(),
 });
