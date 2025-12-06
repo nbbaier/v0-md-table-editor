b17264d079e2889c88206411999da6c0f376a9ab
Nico Baier <nico.baier@gmail.com>
2025-12-06 13:04:41 -0600

    update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,21 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+<<<<<<< Updated upstream
 	integrations: [react()],
 	vite: {
 		plugins: [tailwindcss()],
 	},
+||||||| Stash base
+	integrations: [
+		react(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
+=======
+	integrations: [react()],
+>>>>>>> Stashed changes
 
 	output: "server",
 	adapter: vercel(),
